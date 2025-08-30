a46f3c3e71f9b625718f8dcf513fbb5a9678f646
Danvendra <danvendra480@gmail.com>
2025-08-30 22:01:48 +0530

    changing app.js from the file which has the one with the build folder references

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../client/build')));
-// Only serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/disaster_management', {
@@ -135,17 +129,6 @@ app.put('/api/incidents/:id', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-// Only serve React app in production
-if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
